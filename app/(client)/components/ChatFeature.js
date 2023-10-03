8792a9e7f109428de628eb7738e82ccbf7b4a66e
silentpalms <silentpalmsvilla.com>
2023-10-03 09:16:06 +0300

    edited chat feature

---
+++ app/(client)/components/ChatFeature.js
@@ -0,0 +1,14 @@
+"use client"
+
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
+const ChatFeature = () => {
+  return (
+    <div><TawkMessengerReact
+    propertyId="651bac2110c0b2572487f618"
+    widgetId="1hbq1gpbi"
+  /></div>
+  )
+}
+
+export default ChatFeature
--- app/(client)/layout.js
+++ app/(client)/layout.js
@@ -1,18 +1,16 @@
 import "leaflet/dist/leaflet.css";
-import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import ChatFeature from "./components/ChatFeature";
 
 export default function ClientLayout({ children }) {
   return (
     <div>
       
       <Navbar/>
-      <TawkMessengerReact
-          propertyId="651bac2110c0b2572487f618"
-          widgetId="1hbq1gpbi"
-        />
+      <ChatFeature/>
       
       {children}
       <Footer/>
